ae61230ed6495b96cf4dd79260a0a3603e3687ef
wember <58532955+wember@users.noreply.github.com>
2025-11-11 19:59:05 +0900

    Update irr_sim.py

--- creutz-sim/irr_sim.py
+++ creutz-sim/irr_sim.py
@@ -79,8 +79,8 @@ for M in range(m):
             with open(temp_file, 'r') as f:
                 reader = csv.reader(f)
                 next(reader)  # Skip the header row
-                print(len(list(reader)))
                 data = np.array(list(reader), dtype=float)
+                print(data)
             new_row = [i+1, np.mean(data[:, 0]), np.mean(data[:, 1]), np.mean(data[:, 2]), np.mean(data[:, 3]), np.mean(data[:, 4]), np.mean(data[:, 5])/1000, n]
             add_row(file_path, new_row)
 
